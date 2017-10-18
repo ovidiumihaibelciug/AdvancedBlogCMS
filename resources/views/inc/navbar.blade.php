8c4cb0afff25cb11b6961e19de772f09bc923b0a
Ovidiu-Mihai <ovidiumihaibelciug@gmail.com>
2017-10-18 18:52:23 +0300

    Styling the navbar

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,28 @@
+<nav class="navbar is-transparent" id="navbar" style="border-bottom: #d3e0e9 1px solid; max-height: 4.5rem">
+    <div class="container">
+        <div class="navbar-start">
+            <div class="navbar-item">
+                <img id="imgnavbar" src="{{ asset('images/logonegru.png') }}" style="max-height: 3.5rem;">
+            </div>
+            <a href="#" class="navbar-item is-hoverable is-tab is-hidden-mobile">Learn</a>
+            <a href="#" class="navbar-item is-hoverable is-tab is-hidden-mobile">Discuss</a>
+            <a href="#" class="navbar-item is-tab is-hidden-mobile">Share</a>
+        </div>
+
+        <div class="navbar-end">
+            <div class="navbar-item is-hoverable has-dropdown">
+                <a href="#" class="navbar-link">Ovi </a>
+
+                <div class="navbar-dropdown">
+                    <a href="#" class="navbar-item">Profile</a>
+                    <a href="#" class="navbar-item">Notifications</a>
+                    <a href="#" class="navbar-item">Setting</a>
+                    <hr class="navbar-divider">
+                    <a href="#" class="navbar-item">Log out</a>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</nav>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,4 @@
+@include('inc.head')
+    @include('inc.navbar')
+    @yield('content')
+@include('inc.footer')
